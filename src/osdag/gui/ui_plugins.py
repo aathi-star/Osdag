d4ef3e51509e9933be7cd71c064131466c36b65c
unknown <adityarocksgo@gmail.com>
2025-04-17 20:03:26 +0530

    modified ui for list view of pluigns

--- src/osdag/gui/ui_plugins.py
+++ src/osdag/gui/ui_plugins.py
@@ -5,9 +5,7 @@ from importlib.resources import files
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(400, 200)
-        
-        # Set window title and icon
+        MainWindow.resize(600, 400)  
         MainWindow.setWindowTitle("Plugins")
         MainWindow.setWindowIcon(QtGui.QIcon(str(files("osdag.data.ResourceFiles.images").joinpath("Osdag.png"))))
         
@@ -15,25 +13,25 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
         MainWindow.setCentralWidget(self.centralwidget)
 
-        # Main layout
+        #Main layout
         self.layout = QtWidgets.QVBoxLayout(self.centralwidget)
         self.layout.setObjectName("layout")
 
-        # Add status label
-        self.status_label = QtWidgets.QLabel("Select a plugin to activate")
+        #status label
+        self.status_label = QtWidgets.QLabel("Select plugins to activate")
         self.status_label.setObjectName("status_label")
         self.layout.addWidget(self.status_label)
 
-        # Add plugin combo box
-        self.plugin_combo = QtWidgets.QComboBox(self.centralwidget)
-        self.plugin_combo.setObjectName("plugin_combo")
-        self.plugin_combo.addItem("Select plugin")
-        self.layout.addWidget(self.plugin_combo)
+        #plugin list 
+        self.plugin_list = QtWidgets.QListWidget(self.centralwidget)
+        self.plugin_list.setObjectName("plugin_list")
+        self.plugin_list.setSelectionMode(QtWidgets.QAbstractItemView.MultiSelection)
+        self.layout.addWidget(self.plugin_list)
 
-        # Add activate button
-        self.activate_button = QtWidgets.QPushButton("Activate Plugin", self.centralwidget)
+        #activate button
+        self.activate_button = QtWidgets.QPushButton("Activate Selected Plugins", self.centralwidget)
         self.activate_button.setObjectName("activate_button")
         self.layout.addWidget(self.activate_button)
 
-        # Add spacing at the bottom
+        #spacing
         self.layout.addStretch()
